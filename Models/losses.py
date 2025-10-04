5a341cd438aefa3e60cea28e82c341d43452f5d5
Chenhaoying <2895724421@qq.com>
2025-10-04 12:53:51 +0800

    Update losses.py

--- Models/losses.py
+++ Models/losses.py
@@ -7,20 +7,15 @@ def cross_entropy_loss(proportions, device):
     counts = torch.tensor(proportions, dtype=torch.float32)
     inv_freq = 1.0 / counts
     weights = inv_freq / inv_freq.sum()
+    
     return nn.CrossEntropyLoss(weight=weights).to(device)
 
-class JointLoss(nn.Module):
-    """
-    JointLoss: α * CrossEntropyLoss + β * MSELoss
-    """
-    def __init__(self, proportions, coef1, coef2, device):
-        super().__init__()
-        self.coef1 = coef1
-        self.coef2 = coef2
-        self.mse = nn.MSELoss()
-        self.ce = cross_entropy_loss(proportions, device)
+import torch
+import torch.nn as nn
 
-    def forward(self, logit, tgt, pred, real):
-        assert len(logit.shape) == 2
-        assert pred.shape == real.shape
-        return self.coef1 * self.ce(logit, tgt) + self.coef2 * self.mse(pred, real)
+def jointloss(proportions, coef1, coef2, device):
+    return cross_entropy_loss(proportions, device), nn.MSELoss().to(device)
+
+
+
+    
